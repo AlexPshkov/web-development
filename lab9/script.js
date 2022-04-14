24410a58b0157f080deeb26e4d00508daf461f44
AlexPshkov <alexpshkov@gmail.com>
2022-04-14 07:00:03 +0300

    lab 9

--- lab9/script.js
+++ lab9/script.js
@@ -4,12 +4,12 @@
  * @param number
  */
 function isPrimeNumber(number) {
-    if (number instanceof Array) { //Handle array
+    if (number instanceof Array) {
         number.forEach(num => isPrimeNumber(num));
         return;
     }
-    if (number % 2 === 0 && number > 2) return console.log(number + " is not prime number"); //Simple check
-    for (let i = 3; i <= Math.sqrt(number); i += 2) { //Main check
+    if (number % 2 === 0 && number > 2) return console.log(number + " is not prime number");
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
         if (number % i === 0) return console.log(number + " is not prime number");
     }
     return console.log(number + " is prime number");
@@ -89,4 +89,4 @@ function calc(expression, checked = false) {
 }
 
 
-console.log(calc('*(- 5 6) (* (+ -2 -5)  (+    1 1))'));
+// console.log(calc('*(- 5 6) (* (+ -2 -5)  (+    1 1))'));
